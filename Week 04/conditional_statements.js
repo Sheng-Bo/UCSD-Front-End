96f3fe2a4ad8cd6117fd48695c8474442c0f2dbe
Sheng-Bo Chen <artalose@gmail.com>
2023-01-30 09:24:17 -0500

    Week 04

---
+++ Week 04/conditional_statements.js
@@ -0,0 +1,9 @@
+if (condition1) {
+    // code to execute if condition1 is true
+} else if (condition2) {
+    // code to execute if condition1 is false and condition2 is true
+} else if (condition3) {
+    // code to execute if condition1 and condition2 are false and condition3 is true
+} else {
+    // code to execute if all previous conditions are false
+}
